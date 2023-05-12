74681ee3dccdfc77cb8fffab3a26744891c5504c
j-mags <112118868+j-mags@users.noreply.github.com>
2023-05-12 03:42:31 +0000

    Finetuning intro

--- main.py
+++ main.py
@@ -1,48 +1,52 @@
 #agh. figure out uhhhhh... like using objects 
 #put items in each room
 def gameplay():
-	print("---WELCOME---\n")
-	print("\tAmidst bubbling potions and enchanting spells, a mystery occurs at your school;\n\tit's your duty to solve the puzzles and recover what has gone missing.")
-	run.py()
-	start = True
-rooms = {"Library", "Hallway", "Classroom", "Greenhouse"}
-location = ""
-while start == True:
-	try:
-		location = rooms[0]
-		while location == rooms[0]:
-			print(f{room_descriptions[0]})
-			print("A girl runs up to you, startling you so hard that you almost drop your book.\n")
-			qna = int(input('''"Hey, you're the student council president, right?"\n\t> Yeah, what's up? (1)\n\t> Why are you asking if you already know… (2)\n'''))
-			friendship = 0
-			try:
-			qna = int(qna)
-				if qna != 1 or qna != 2:
-					print("Sorry, that wasn’t a valid option.\n")
-			except:
-				if qna == 1:
-					print('''\t> "Perfect! The head librarian has a bit of a... problem we need to fix."''')
-			else:
-				if qna == 2:
-					print('''She frowns. "Just formalities, I guess."\n "The head librarian has a problem, and she said I needed to find you." (-1 Friendship Points)\n''')
-				friendship -= 1
-			print("Friendship Meter: [{"*"*{friendship}}]")
-			print('''"Apparently, one of the books from the forbidden section of the library has gone missing."\n"Not sure why we have a 'forbidden' section here to begin with."''')
-			if friendship < 0:
-				print('''She sighs. "I guess we'll have to find it together."''')
-			else:
-				print('''"Looks like we'll be partners in crime today!"\n''')
-			print("Your colored pencil (weird that you only have one) is sitting on a desk.")
-			game.input()
+	rooms = {"Library", "Hallway", "Classroom", "Greenhouse"}
+	print(f"{room_descriptions[0]}")
+	print("A girl runs up to you, startling you so hard that you almost drop your book.\n")
+	print('''[?]: "Hey, you!"''')
+	name = input('''[?]: "Um... you, uh.."\n[?]: "I'm sorry- what was your name again?"\n''')
+	print(f"\t> It's {name}.\n")
+	print(f'''[?]: "Right! Nice to meet you, {name}! I'm Haruna."\n[Haruna]: "Names aside, you're the student council president, right?"''')
+	qna = int(input('''\n\t> Yeah, what's up? (1)\n\t> Why are you asking if you already know… (2)\n'''))
+	friendship = 0
+	if qna == 1:
+		print('''\t[Haruna]: "Perfect! The head librarian has a bit of a... problem we need to fix."''')
+	elif qna == 2:
+		print('''She frowns. "Just formalities, I guess."\n"The head librarian has a problem, and she said I needed to find you." (-1 Friendship Points)\n''')
+		friendship -= 1
+		enter = input("Friendship Level: [-1]\n\tPress enter to continue.")
+	print('''\n\n[Haruna]: "Apparently, one of the books from the forbidden section of the library has gone missing.\nNot sure why we have a 'forbidden' section here to begin with..."''')
+	if friendship < 0:
+		print('''\nShe sighs. "I guess we'll have to find it together."''')
+	else:
+		print('''[Haruna]: "Looks like we'll be partners in crime today!"\n''')
+		friendship += 1
+		enter = input("Friendship Level: [1]")
+	examine()
+def examine():
+	look = input("\nWould you like to examine the room?\n\t> Yes\n\t> No\n")
+	if look in ("Y","Yes","y","yes"):
+		print("Your colored pencil (weird that you only have one) is sitting on a desk.")
+		choice()
+	else:
+		yikes = input("Okay... um.")
+		yikes = input("Nice weather we're having here.")
+		yikes = input("Alright let's get back to the game.")
+		examine()
+def choice():
+	actions = ("quit", "save", "load", "get", "use")
+	reponse = ""
+	response = ("What would you like to do?\n")
+	while response not in actions:
+		print(f"Choices:\n{actions}\n")
+		response = input(response).lower()
+	return response
 
-
-
-
-
-room_description = {"The library is expansive, with the smell of cinnamon and the fluttering of pages in the air.", 
+room_descriptions = ["The library is expansive, with the smell of cinnamon and the fluttering of pages in the air.", 
 "It's bustling in the hallway. You can't help but bump into passerbys.", 
 "The decor in the potion-brewing classroom is eclectic.\nThere is a permanent glow of dust in the air from the amount of failed potions by students.",
-"Navigating through lush greenery, you spot the plant you've been searching for."}
+"Navigating through lush greenery, you spot the plant you've been searching for."]
 
 class Room():
 	def __init__(self):
@@ -61,26 +65,3 @@ class Room():
 #loop for movement
 #add objects
 #make inventory
-	actions = ("quit", "save", "load", "get", "use")
-
-def input():
-	response = ("What would you like to do?")
-	response = None
-	while response not in actions:
-		print(f"Choices:\n{actions}")
-		response = input(prompt).lower()
-	return response
-player = Player()
-def main(player):
-	choice = None
-	while choice != "quit":
-		if choice == "quit":
-			print("Come back soon! We have a book to find.")
-		elif choice == "save":
-			save()
-		elif choice == "load":
-			load()
-		elif choice == "get":
-			room.get_item(player)
-		elif choice == "use":
-			room.use_item(player)
--- run.py
+++ run.py
@@ -1,12 +1,15 @@
-import game
+import main
 quit = ('quit','q')
 start = ('start','s')
 choice = ''
 while choice not in quit:
-	choice = input("Would you like to begin?\n").lower()
-	print("Start (s)\nQuit (q)")
-		if choice in start:
-			print("Good luck!")
-			project.game()
-		elif choice in quit:
-			print("See you soon.")
+	print("\n\t\t\t---------------WELCOME---------------\n")
+	print("\tAmidst bubbling potions and enchanting spells, a mystery occurs at your school;\n\tit's your duty to solve the puzzles and recover what has gone missing.")
+	choice = input("\n\tWould you like to begin?\n\t> Start (s)\n\t> Quit (q)\n").lower()
+	if choice in start:
+		print("Good luck!\n---\n")
+		main.gameplay()
+	elif choice in quit:
+		print("See you soon.")
+	else:
+		print("There's only two choices, buddy. Ahem. Let's restart.")
